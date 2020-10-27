1bb07892f57251b45232c0814d0f36de41ff6894
André Sebastian-Bath <a.sebastian@posteo.de>
2020-10-27 21:42:01 +0100

    avoid timeout

--- util.test.js
+++ util.test.js
@@ -18,8 +18,7 @@ test('should generate a valid text output', () => {
 
 test('should click around', async () => {
     const browser = await puppeteer.launch ({
-        headless: false,
-        slowMo: 80,
+        headless: true,
         args: ['---windows-size=1920,1080']
     })
 
@@ -31,4 +30,6 @@ test('should click around', async () => {
     await page.click('input#age')
     await page.type('input#age', '28')
     await page.click('#btnAddUser')
-})
+    const finalTest = await page.$eval('.user-item', el => el.textContent)
+    expect(finalTest).toBe('Anna (28 years old)')
+}, 15000)
